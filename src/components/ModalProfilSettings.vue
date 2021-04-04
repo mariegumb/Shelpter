fc433a27158396502aef5acea5dbdf7f105bf5bc
Lucasben <Lucasbenyamin@users.noreply.github.com>
2021-04-04 18:11:59 +0200

    Modif profil / changement de profil(en cours)

--- src/components/ModalProfilSettings.vue
+++ src/components/ModalProfilSettings.vue
@@ -5,6 +5,108 @@
         </ion-toolbar>
     </ion-header>
     <ion-content class="ion-padding">
+<!-- This example requires Tailwind CSS v2.0+ -->
+    <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+        <div>
+            <div class="mt-3 ml-3 flex items-center">
+                <span class="inline-block h-1/2 w-1/2 rounded-full overflow-hidden bg-gray-100">
+                    <svg class="z-0 h-full w-full text-purple-600" fill="currentColor" viewBox="0 0 24 24">
+                        <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                    </svg>
+                    <button type="button" class="z-10 flex p-2 rounded-md hover:bg-purple-600 focus:ring-2">
+                        <svg class="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                        </svg>
+                    </button>
+                </span>
+            </div>
+        </div>
+    <div class="border-t border-gray-200">
+        <dl>
+        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">
+            Full name
+            </dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+            Margot Foster
+            </dd>
+        </div>
+        <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">
+            Application for
+            </dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+            Backend Developer
+            </dd>
+        </div>
+        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">
+            Email address
+            </dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+            margotfoster@example.com
+            </dd>
+        </div>
+        <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">
+            Salary expectation
+            </dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+            $120,000
+            </dd>
+        </div>
+        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">
+            About
+            </dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+            Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
+            </dd>
+        </div>
+        <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">
+            Attachments
+            </dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+            <ul class="border border-gray-200 rounded-md divide-y divide-gray-200">
+                <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                <div class="w-0 flex-1 flex items-center">
+                    <!-- Heroicon name: solid/paper-clip -->
+                    <svg class="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
+                    </svg>
+                    <span class="ml-2 flex-1 w-0 truncate">
+                    resume_back_end_developer.pdf
+                    </span>
+                </div>
+                <div class="ml-4 flex-shrink-0">
+                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
+                    Download
+                    </a>
+                </div>
+                </li>
+                <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                <div class="w-0 flex-1 flex items-center">
+                    <!-- Heroicon name: solid/paper-clip -->
+                    <svg class="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
+                    </svg>
+                    <span class="ml-2 flex-1 w-0 truncate">
+                    coverletter_back_end_developer.pdf
+                    </span>
+                </div>
+                <div class="ml-4 flex-shrink-0">
+                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
+                    Download
+                    </a>
+                </div>
+                </li>
+            </ul>
+            </dd>
+        </div>
+        </dl>
+    </div>
+    </div>
         <div style="display:flex;justify-content:center;">
             <ion-button @click="onCancel" color="light">Cancel</ion-button>
             <ion-button @click="onApply">Apply</ion-button>
--- src/views/Profil.vue
+++ src/views/Profil.vue
@@ -2,6 +2,38 @@
     <ion-page>
         <Header/>
         <ion-content :fullScreen="true">
+          <div class="bg-purple-600">
+            <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+              <div class="flex items-center justify-between flex-wrap">
+                <div class="w-0 flex-1 flex items-center">
+                  <span class="flex p-2 rounded-lg bg-indigo-800">
+                    <!-- Heroicon name: outline/speakerphone -->
+                    <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
+                    </svg>
+                  </span>
+                  <p class="ml-3 font-medium text-white truncate">
+                    <span class="md:hidden">
+                      Soutenez nous ! 
+                    </span>
+                  </p>
+                </div>
+                <div class="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+                  <a @click="openDonation" class="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-purple-600 bg-white hover:bg-purple-50">
+                    Faire un don
+                  </a>
+                </div>
+                <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
+                  <button type="button" class="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">
+                    <!-- Heroicon name: outline/x -->
+                    <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
           <div>
             <div class="flex w-full">
               <div class="rounded-full h-24 w-24 bg-center bg-cover mt-3 ml-3" id="profil">
@@ -10,14 +42,16 @@
                   <div class="flex justify-center">
                     <div CLASS="w-1/12"></div>
                     <div CLASS="w-10/12 text-center">
-                      <span class="text-xl inline-block bg-purple-100 text-purple-600 py-1 px-2 rounded-lg ">
+                      <span class="text-xl inline-block bg-purple-100 text-yellow-500 py-1 px-2 rounded-lg ">
                       Mini Justicier
                     </span>
                     </div>
-                    <div class="w-1/12 flex justify-end">
-                      <svg class="w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                    <div class="w-1/12 mr-2 flex justify-end">
+                    <button @click="openSettings" type="button" class="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">
+                      <svg class="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                       </svg>
+                    </button>
                     </div>
                   </div>
                 <div class="flex pt-4 justify-center">
@@ -40,10 +74,7 @@
               <span>Caroline</span>
             </div>
           </div>
-          <ion-button  @click="deconnect" color="purple">Déconnexion</ion-button>
-          <ion-button  @click="openSettings" color="purple">Modifier le profil</ion-button>
-          <ion-button  @click="openDonation" color="purple">Faire un don</ion-button>
-          
+          <ion-button @click="deconnect" color="purple" class="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-semibold text-white bg-purple-600 hover:bg-purple-50">Déconnexion</ion-button>         
         </ion-content>
     </ion-page>
 </template>
